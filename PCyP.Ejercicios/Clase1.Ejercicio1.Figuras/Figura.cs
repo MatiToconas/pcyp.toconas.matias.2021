d83fc5dd9f5a94e3683fdb7ee13ff07bb42a827d
Matías Nicolás <matias.nicolas3007@gmail.com>
2021-03-30 23:33:38 -0300

    Implementar cuadrado
    Implementar area y perimetro del cuadrado

---
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Figura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio1.Figuras
+{
+    public class Figura
+    {
+        private List<int> lados;
+
+        public int Perimetro()
+        {
+            return 0;
+        }
+
+        public int Area()
+        {
+            return 0;
+        }
+    }
+}
---
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio1.Figuras
+{
+    /// <summary>
+    /// Realizar un programa capaz de calcular el área y perímetro de un cuadrado, rectángulo y triángulo. 
+    /// Todos los datos necesarios para su funcionamiento se ingresan por teclado.
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Figura fig = new Figura();
+            fig.Perimetro();
+        }
+    }
+}
