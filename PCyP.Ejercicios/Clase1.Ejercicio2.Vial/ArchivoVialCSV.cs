fdccfb0c364d48dc2654a02c894391feed75f662
Matías Nicolás <matias.nicolas3007@gmail.com>
2021-04-05 00:31:53 -0300

    Ejercicio vial implementado
    Se recorre el archivo CSV

---
+++ PCyP.Ejercicios/Clase1.Ejercicio2.Vial/ArchivoVialCSV.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio2.Vial
+{
+    class ArchivoVialCSV
+    {
+        private int numHoja;
+        private int idTramo;
+        private int longitud;
+        private string tipObj;
+        private string tipCamino;
+
+        public ArchivoVialCSV()
+        {
+            numHoja = 0;
+            idTramo = 0;
+            longitud = 0;
+            tipObj = null;
+            tipCamino = null;
+        }
+
+        public ArchivoVialCSV(int numeroHoja, string tipoObje, int idTram, string tipoCamino, int lon)
+        {
+            numHoja = numeroHoja;
+            tipObj = tipoObje;
+            idTramo = idTram;
+            tipCamino = tipoCamino;
+            longitud = lon;
+        }
+
+        public void imprimirLinea()
+        {
+            Console.WriteLine("Número de hoja: {0}\t Tipo de objeto: {1}\t Identificador de tramo: {2}\t Tipo de camino: {3}\t Longitud: {4}", numHoja, tipObj, idTramo, tipCamino, longitud);
+        }
+    }
+}
--- PCyP.Ejercicios/Clase1.Ejercicio2.Vial/Program.cs
+++ PCyP.Ejercicios/Clase1.Ejercicio2.Vial/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Clase1.Ejercicio2.Vial
 {
@@ -10,7 +11,20 @@ namespace Clase1.Ejercicio2.Vial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
+            string[] lineas = File.ReadAllLines("C:/Users/Toconás Matías/Documents/GitHub/pcyp.toconas.matias.2021/PCyP.Ejercicios/Clase1.Ejercicio2.Vial/vial.csv");
+
+            ArchivoVialCSV filas = new ArchivoVialCSV();
+            int contador = 0;
+
+            foreach (var linea in lineas)
+            {
+               var valor = linea.Split(',');
+
+                filas = new ArchivoVialCSV(int.Parse(valor[0]), valor[1], int.Parse(valor[2]), valor[3], int.Parse(valor[4]));
+                filas.imprimirLinea();
+                contador++;   
+             }
+            Console.WriteLine("\nRegistro de valores: " + contador);
             Console.ReadLine();
         }
     }
