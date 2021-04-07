2af2af419d37f0c126e416fd35f7f8a93006b581
Matías Nicolás <matias.nicolas3007@gmail.com>
2021-04-07 00:12:36 -0300

    Implementar diccionario
    Manejo de diccionario

---
+++ PCyP.Ejercicios/Clase3.Ejercicio1.Diccionario/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejercicio1.Diccionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
