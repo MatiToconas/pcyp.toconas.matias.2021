02953494edff0fae67eeed538cb2afd2f7ee99ca
Matías Nicolás <matias.nicolas3007@gmail.com>
2021-04-08 00:55:58 -0300

    Desarrollo ej1, ej2, ej3
    Ejercicio 4 pendiente

--- PCyP.Ejercicios/Clase3.Ejercicio1.Diccionario/Program.cs
+++ PCyP.Ejercicios/Clase3.Ejercicio1.Diccionario/Program.cs
@@ -14,6 +14,42 @@ namespace Clase3.Ejercicio1.Diccionario
             values.Add("Juan", "55423412");
             values.Add("Ernesto", "56985623");
             values.Add("Mariana", "54787451");
+
+            //a)
+           if (values.ContainsKey("Juan"))
+           {
+                Console.WriteLine(values["Juan"]); 
+                Console.ReadKey();
             }
+
+            //b)
+            string nombre;
+
+            if (values.TryGetValue("Pedro", out nombre))
+            {
+                Console.WriteLine(nombre);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No contiene la llave");
+                Console.ReadKey();
+            }
+
+            //c)
+            
+            foreach (KeyValuePair<string, string> entrada in values)
+            {
+                Console.WriteLine("{0}, {1}", entrada.Key, entrada.Value);
+            }
+            Console.ReadKey();
+
+            //d)
+           
+            values["Mariana"] = "58251425";
+            Console.WriteLine("Nuevo teléfono de Mariana: {0}", values["Mariana"]);
+            Console.ReadKey();
+
+        }
     }
 }
---
+++ PCyP.Ejercicios/Clase3.Ejerecicio2.Listas/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Ejerecicio2.Listas
+{
+    class Program
+    {
+        private static readonly string[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+        private static readonly string[] removeColors = { "RED", "WHITE", "BLUE" };
+
+        static void Main(string[] args)
+        {
+
+            List<string> list = new List<string>();
+
+            // Añade elementos de colors a la Lista.
+            foreach (var color in colors)
+            {
+                list.Add(color);
+            }
+
+            // Añade elementos del arreglo desde el constructor
+            List<string> removeList = new List<string>(removeColors);
+
+            Console.WriteLine("Lista original de colores: ");
+            MostrarLista(list);
+
+            // remueve colores
+            RemueveColores(list, removeList);
+
+            Console.WriteLine("\nLista con los colores removidos (RED, WHITE, BLUE): ");
+            MostrarLista(list);
+
+        }
+
+        private static void MostrarLista(List<string> list)
+        {
+            // muestra el contenido con foreach
+            foreach (var element in list)
+            {
+                Console.Write("{0} ", element);
+            }
+            Console.ReadKey();
+        }
+
+        private static void RemueveColores(List<string> firstList, List<string> secondList)
+        {
+            for (int count = 0; count < secondList.Count; ++count)
+            {
+                firstList.Remove(secondList[count]);
+            }
+        }
+    }
+  }
+
