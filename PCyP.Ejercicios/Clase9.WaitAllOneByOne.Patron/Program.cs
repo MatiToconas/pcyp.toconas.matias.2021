8f98fdfcbdbcad811fc7412b781c105884f730c4
Matías Nicolás <matias.nicolas3007@gmail.com>
2021-06-24 00:39:55 -0300

    WaitAllOneByOne
    Se implementa patron

---
+++ PCyP.Ejercicios/Clase9.WaitAllOneByOne.Patron/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using Lib;
+
+namespace Lib
+{
+    public abstract class ConsoleProgram
+    {
+        public abstract void run();
+
+        public void showMsg(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public string getString(string msg)
+        {
+            showMsg(msg);
+            return Console.ReadLine();
+        }
+
+        public void pause()
+        {
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
+
+namespace WaitAllOneByOne
+{
+       class Program : ConsoleProgram
+    {
+        static void Main (string[] args)
+        {
+            Program program = new Program ();
+            program.run ();
+            program.pause ();
+        }
+
+        override
+        public void run () {
+            List<Task<string>> tasks = new List<Task<string>> ();
+            for (int i=0;i<10;i++) {
+                tasks.Add(new Task<string>((index) => {
+                    Thread.SpinWait (10000000);
+                    return "Tarea "+index+" finalizada.";
+                }, i));
+            }
+            foreach(Task t in tasks){
+                t.Start();
+            }
+            while(tasks.Count>0){
+                int index = Task.WaitAny(tasks.ToArray());
+                showMsg("Result: "+tasks.ToArray()[index].Result);
+                tasks.RemoveAt(index);
+            }
+            showMsg ("Hilo principal esperó a las tareas una por una");
+        }
+    }
+}
+
